0a29a0663a51f68f2cb1e4f09ebb3c8deea521e8
notaika <ercamanalo@gmail.com>
2024-05-23 08:27:31 -0700

    fix: renamed the files to seed instead of seed data (oversight sorry). feat: added delete endpoint and connected to database

--- controllers/warehouse-controller.js
+++ controllers/warehouse-controller.js
@@ -36,7 +36,22 @@ async function getWarehouse(req, res) {
   }
 }
 
+async function deleteWarehouse(req, res) {
+  const { id } = req.params;
+  try {
+    const data = await knex('warehouses').where('id', id).del();
+    if (!data.length) {
+      res.sendStatus(404)
+    } else {
+      res.status(200).json(data)
+    }
+  } catch (err) {
+    res.status(400).send(`Error retrieving warehouse: ${err}`)
+  }
+}
+
 module.exports = {
   getWarehouses,
   getWarehouse,
+  deleteWarehouse
 };
--- routes/warehouse.js
+++ routes/warehouse.js
@@ -4,9 +4,10 @@ const warehouseController = require("../controllers/warehouse-controller");
 const fs = require('fs');
 
 router.route('/')
-    .get(warehouseController.getWarehouses);
+    .get(warehouseController.getWarehouses)
 
 router.route("/:id")
-    .get(warehouseController.getWarehouse);
+    .get(warehouseController.getWarehouse)
+    .delete(warehouseController.deleteWarehouse)
 
 module.exports = router;
