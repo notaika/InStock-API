65f47f0d8fcf22a0e6d967fd91539f3bdc92201d
Rinu2024 <158245876+Rinu2024@users.noreply.github.com>
2024-05-23 18:29:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/warehouses", warehouseRoutes);
-app.use("/api/inventory", inventoryRoutes);
+app.use("/api/inventories", inventoryRoutes);
 
 app.get("/", (req, res) => {
   res.send("Landed on the server");
